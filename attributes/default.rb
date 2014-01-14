c75ba1fc4c88882f0dce18fc5f4ff0a41eb6ec5e
poppen <poppen.jp@gmail.com>
2014-01-15 08:45:36 +0900

    update srpm url

--- attributes/default.rb
+++ attributes/default.rb
@@ -22,5 +22,5 @@
 default['lv']['version'] = '4.51'
 
 default['lv']['rpm_version'] = '8.1'
-default['lv']['srcrpm_url'] = 'http://ftp.riken.jp/Linux/centos/5.6/os/SRPMS'
+default['lv']['srcrpm_url'] = 'http://vault.centos.org/5.10/os/SRPMS'
 default['lv']['srcrpm_checksum'] = '740ce98310c6324057fe82ee7392db6536fffc4e'
