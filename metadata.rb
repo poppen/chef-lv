ef92ee4ccd50041af5e4411afb80f4fa43236955
poppen <poppen.jp@gmail.com>
2014-01-14 21:40:20 +0900

    update and bump version

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,13 @@
 name             'lv'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures lv'
+maintainer       'MATSUI Shinsuke'
+maintainer_email 'poppen.jp@gmail.com'
+license          'Apache 2.0'
+description      'Installs lv'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
+
+recipe 'lv', 'Installs lv'
+
+%w{ centos ubuntu }.each do |os|
+    supports os
+end
